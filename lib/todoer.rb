b02ebe3bc49649d9f3d9e863161dcadfa264f0da
Hyagobsantos <hyagobezerra64@gmail.com>
2023-03-23 17:16:40 -0300

    initial commit 😇

---
+++ lib/todoer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "todoer/version"
+
+module Todoer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/todoer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Todoer do
+  it "has a version number" do
+    expect(Todoer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
